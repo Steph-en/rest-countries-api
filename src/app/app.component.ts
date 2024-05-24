207bcf8442dd9dc95342971213a95a25f0c22430
Stephen Appah <appahstephen9@gmail.com>
2024-05-24 10:05:51 +0000

    Feat: Changed moon svg to fontawesome icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HomeComponent]
+    imports: [RouterOutlet, HomeComponent, FontAwesomeModule]
 })
 export class AppComponent implements OnInit{
   title = 'rest-countries-api';
+  moon = faMoon;
 
   constructor() {}
 
