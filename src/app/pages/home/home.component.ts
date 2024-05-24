339193c993933544563d37e9504cb56b39c3aefc
Stephen Appah <appahstephen9@gmail.com>
2024-05-24 11:18:11 +0000

    Feat: Added an arrow icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  search = faSearch;
+  arrow = faChevronDown;
 }
