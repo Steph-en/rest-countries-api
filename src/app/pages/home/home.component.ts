53b040b09dd5f773a3f225b87429db64138d36df
Steph-en <appahstephen9@gmail.com>
2024-05-24 15:35:14 +0000

    Woking on card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { CountryComponent } from "../../country/country.component";
 import { DetailsComponent } from "../details/details.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [FontAwesomeModule, DetailsComponent]
+    imports: [FontAwesomeModule, DetailsComponent, RouterOutlet, CountryComponent]
 })
 export class HomeComponent {
   search = faSearch;
