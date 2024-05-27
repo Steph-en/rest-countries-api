56210c9aef192711c082e76251625339c36a4993
Stephen Appah <appahstephen9@gmail.com>
2024-05-27 09:40:05 +0000

    Feat: working on cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,25 +2,24 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { DetailsComponent } from "../details/details.component";
+import { CountryComponent } from "../../country/country.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [FontAwesomeModule, DetailsComponent]
+    imports: [FontAwesomeModule, DetailsComponent, CountryComponent]
 })
 export class HomeComponent {
   search = faSearch;
   arrow = faChevronDown;
 
   toggle() {
-    const dropBtn = document.querySelector(".filter-button") as HTMLElement;
     const dropDown = document.querySelector(".drop-down") as HTMLElement;
 
-    if (dropBtn && dropDown) {
+    if (dropDown) {
       dropDown.style.display = dropDown.style.display === 'block' ? 'none' : 'block';
-
     }
   }
 }
