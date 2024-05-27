b61e45cb5e6dcfb7353cda37b3d013c45b81def4
Steph-en <appahstephen9@gmail.com>
2024-05-27 14:48:25 +0000

    working on services

--- src/app/interface/interface.ts
+++ src/app/interface/interface.ts
@@ -1,2 +1,36 @@
 export interface Interface {
+    name: {
+        common: string;
+        nativeName: {
+            [key: string]: {
+                official: string;
+                common: string;
+            };
+        };
+        official: string;
+    };
+    capital: string;
+    population: number;
+    region: string;
+    subregion: string;
+    tld: string;
+    languages: {
+        [key: string]: {
+            name: string;
+            nativeName: string;
+        };
+    };
+    currencies: {
+        [key: string]: {
+            name: string;
+            symbol: string;
+        };
+    };
+    borders: string[];
+    flags: {
+        png: string;
+        svg: string;
+        alt: string;
+    };
+    cca3: string;
 }
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Interface } from '../interface/interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  private countryAPI = 'https://restcountries.com/v3.1/all'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCountryData(): Observable<Interface[]> {
+    return this.http.get<Interface[]>(this.countryAPI);
+  }
+  
+  logAllCountryData(): void {
+    this.getAllCountryData().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log('Error fetching country data:', error);
+    });
+  }
 }
