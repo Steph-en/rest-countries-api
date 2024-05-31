65424abbcabb08370ccfd5f2a4058803e9d5e6e9
Steph-en <appahstephen9@gmail.com>
2024-05-31 13:01:37 +0000

    Feat: Done with details and countries borders

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { Interface } from '../interface/interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-  private allCountries!: Interface[]; 
-  private countryAPI = 'https://restcountries.com/v3.1/all'
+  private allCountries!: Interface[];
+  private countryAPI = 'https://restcountries.com/v3.1/all';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public getAllCountryData(): Observable<Interface[]> {
-    if(this.allCountries) {
-      return of(this.allCountries)
+    if (this.allCountries) {
+      return of(this.allCountries);
     } else {
       return this.http.get<Interface[]>(this.countryAPI).pipe(
         tap((countries) => (this.allCountries = countries))
-      )
+      );
     }
   }
 
   public getCountryByName(name: string): Observable<Interface[]> {
-    return of(this.allCountries.filter((country) => country.name.common === name))
+    return of(this.allCountries.filter((country) => country.name.common === name));
+  }
+
+  public getCountryByCode(code: string): Observable<Interface[]> {
+    return of(this.allCountries.filter((country) => country.cca3 === code));
   }
 }
