fd6bf3667acbb97020d45c14c99edff59dd872cc
Steph-en <appahstephen9@gmail.com>
2024-06-03 15:00:55 +0000

    Feat: working on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Nunito'],
+      },
+      color: {},
+      screens: {}
+    },
   },
   plugins: [],
 }
