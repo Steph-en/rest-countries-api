448e1c2037a48304482bee9aa3e977099ec02d0c
Steph-en <appahstephen9@gmail.com>
2024-06-07 22:03:11 +0000

    Done testing HomeComponent

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -80,32 +80,26 @@ describe('HomeComponent', () => {
   });
 
   it('should toggle filter', () => {
-    // component.showFilter = false;
-    // component.toggle();
-    // expect(component.showFilter).toBeTrue();
-    // component.toggle();
-    // expect(component.showFilter).toBeFalse();
-
-    pending();
+    component.showFilter = false;
+    component.toggle();
+    expect(component.showFilter).toBeTrue();
+    component.toggle();
+    expect(component.showFilter).toBeFalse();
   });
 
   it('should filter countries by region', () => {
-    // const mockFilteredCountries: Interface[] = [mockCountryList[0]]; // Assuming only the first country belongs to 'Region1'
-    // spyOn(service, 'getFilteredCountriesByRegion').and.returnValue(of(mockFilteredCountries));
-    // component.filterRegion('Region1');
-    // expect(service.getFilteredCountriesByRegion).toHaveBeenCalledWith('Region1');
-    // expect(component.filteredInterfaceList).toEqual(mockFilteredCountries);
-
-    pending();
+    const mockFilteredCountries: Interface[] = [mockCountryList[0]]; // Assuming only the first country belongs to 'Region1'
+    spyOn(service, 'getFilteredCountriesByRegion').and.returnValue(of(mockFilteredCountries));
+    component.filterRegion('Region1');
+    expect(service.getFilteredCountriesByRegion).toHaveBeenCalledWith('Region1');
+    expect(component.filteredInterfaceList).toEqual(mockFilteredCountries);
   });
 
   it('should filter countries by search text', () => {
-    //   const searchText = 'country1'; // Assuming 'Country1' is part of the search text
-    //   component.interfaceList = mockCountryList; // Initialize interfaceList
-    //   component.filterCountries({ target: { value: searchText } } as any); // Simulate event with search text
-    //   expect(component.filteredInterfaceList.length).toEqual(1); // Only 'Country1' should match the search
-    //   expect(component.filteredInterfaceList[0].name.common.toLowerCase()).toContain(searchText);
-    pending();
+      const searchText = 'country1'; // Assuming 'Country1' is part of the search text
+      component.interfaceList = mockCountryList; // Initialize interfaceList
+      component.filterCountries({ target: { value: searchText } } as any); // Simulate event with search text
+      expect(component.filteredInterfaceList.length).toEqual(1); // Only 'Country1' should match the search
+      expect(component.filteredInterfaceList[0].name.common.toLowerCase()).toContain(searchText);
   });
-
 });
