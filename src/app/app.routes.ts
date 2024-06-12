7ea6c5e734d604512855f843412bea87ccf1e6c6
Steph-en <appahstephen9@gmail.com>
2024-06-12 09:49:13 +0000

    Feat: Reamoved HomeComponent import from routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
 
 export const routes: Routes = [
