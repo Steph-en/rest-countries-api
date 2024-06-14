66da439ba592721767aa52278c1febd223b3927e
Stephen Appah <appahstephen9@gmail.com>
2024-06-14 14:07:38 +0000

    Feat: minor tweak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,6 @@ export class AppComponent implements OnInit{
 
   toggle() {
     const theme = document.documentElement.classList.toggle('dark');
-    console.log(theme);
     
     localStorage.setItem("theme", theme ? 'dark' : 'light');
   }
